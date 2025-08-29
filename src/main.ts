7ef3db86e3229946b9f59b347877f7fba1092fcf
mickeyjr <mick007.10@gmail.com>
2025-08-29 14:12:49 -0600

    Ajuste en main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import axios from 'axios';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3003);
 }
+
+async function logServerIP() {
+  try {
+    const res = await axios.get('https://api.ipify.org?format=json');
+    console.log('IP pública del servidor Railway:', res.data.ip);
+  } catch (error) {
+    console.error('No se pudo obtener la IP pública:', error);
+  }
+}
+
+logServerIP();
 bootstrap();
